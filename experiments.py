1de63a52a1d0b25b65e3c945259bc0d37b6395b5
TomerBrown <73110109+TomerBrown@users.noreply.github.com>
2022-06-07 09:54:11 +0300

    Added new experiments and run them.

--- experiments.py
+++ experiments.py
@@ -14,7 +14,7 @@ def experiment1():
     df = pd.read_csv(CSV_PATH)
 
     # Actors Id's are 1-35
-    ids = [i+1 for i in range(35)]
+    ids = [i+1 for i in range(70)]
     random.shuffle(ids)
 
     train_por, val_por, test_por = 0.6, 0.2, 0.2
@@ -31,6 +31,64 @@ def experiment1():
     return train_df, val_df, test_df, name
 
 
+def experiment2():
+    """
+        Creating test set , validation set and test set in the following way:
+        60% of identities in the train set
+        20% of identities in the validation set
+        20% of identities in the test set
+    """
 
+    name = "random_split"
+    df = pd.read_csv(CSV_PATH)
 
+    # Actors Id's are 1-35
+    indices = [i+1 for i in range(len(df))]
+    random.shuffle(indices)
 
+    train_por, val_por, test_por = 0.6, 0.2, 0.2
+    border1, border2 = int(len(df)*train_por) , int(len(df)*(train_por + val_por))
+
+    train_ind = indices[:border1]
+    val_ind = indices[border1:border2]
+    test_ind = indices[border2:]
+
+    train_df = df[df["Unnamed: 0"].isin(train_ind)]
+    val_df = df[df["Unnamed: 0"].isin(val_ind)]
+    test_df = df[df["Unnamed: 0"].isin(test_ind)]
+
+    return train_df, val_df, test_df, name
+
+def experiment3():
+    """
+        Creating test set , validation set and test set in the following way:
+        60% of identities in the train set
+        20% of identities in the validation set
+        20% of identities in the test set
+    """
+
+    name = "leave_out_emotion"
+    df = pd.read_csv(CSV_PATH)
+
+    # Actors Id's are 1-35
+    ids = [i+1 for i in range(70)]
+    emotions = [i for i in range(7)]
+
+    train_df = pd.DataFrame()
+    val_df = pd.DataFrame()
+    test_df = pd.DataFrame()
+    for id in ids:
+        random.shuffle(emotions)
+        emotions_test = emotions[:2]
+        emotions_val = emotions[2]
+        emotions_train = emotions[3:]
+
+        temp_id_df = df[df["actor_id"] == id]
+        train_df = pd.concat([train_df, temp_id_df[temp_id_df["emotion_label"].isin(emotions_train)]])
+        val_df = pd.concat([val_df, temp_id_df[temp_id_df["emotion_label"].isin([emotions_val])]])
+        test_df = pd.concat([test_df, temp_id_df[temp_id_df["emotion_label"].isin(emotions_test)]])
+
+    train_df.to_csv("try_train.csv")
+    val_df.to_csv("try_val.csv")
+    test_df.to_csv("try_test.csv")
+    return train_df, val_df, test_df, name
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from evaluate import evaluate
 
 if __name__ == '__main__':
 
-    train_df, val_df, test_df, name = experiments.experiment1()
-    vgg_model = fine_tune(train_df, val_df, name ,n_epochs=1)
-    evaluate(vgg_model,train_df, val_df, test_df, name)
+    train_df, val_df, test_df, name = experiments.experiment3()
+    #vgg_model = fine_tune(train_df, val_df, name ,n_epochs=1)
+    #evaluate(vgg_model,train_df, val_df, test_df, name)
 
 
